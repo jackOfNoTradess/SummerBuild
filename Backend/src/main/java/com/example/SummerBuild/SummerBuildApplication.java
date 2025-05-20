70fb8cc0d7102eed7ef36d4754be78d4d54acc59
jingronggg <limj0279@e.ntu.edu.sg>
2025-05-20 02:12:07 +0000

    T-008: update tests and main to avoid context loading of whole app

--- Backend/src/main/java/com/example/SummerBuild/SummerBuildApplication.java
+++ Backend/src/main/java/com/example/SummerBuild/SummerBuildApplication.java
@@ -2,11 +2,15 @@ package com.example.SummerBuild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SummerBuildApplication {
-
   public static void main(String[] args) {
-    SpringApplication.run(SummerBuildApplication.class, args);
+    initializeApplication(args);
+  }
+
+  static ConfigurableApplicationContext initializeApplication(String[] args) {
+    return SpringApplication.run(SummerBuildApplication.class, args);
   }
 }
--- Backend/src/test/java/com/example/SummerBuild/controller/AuthControllerTest.java
+++ Backend/src/test/java/com/example/SummerBuild/controller/AuthControllerTest.java
@@ -6,14 +6,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = AuthController.class)
 public class AuthControllerTest {
 
   @Autowired private MockMvc mvc;
