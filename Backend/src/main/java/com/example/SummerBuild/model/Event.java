c9989590ca56b393f9b6d6392add8724f26e59e4
lpwee <pingwee99@gmail.com>
2025-05-21 08:41:42 +0000

    try agn

--- Backend/src/main/java/com/example/SummerBuild/model/Event.java
+++ Backend/src/main/java/com/example/SummerBuild/model/Event.java
@@ -1,18 +1,21 @@
 package com.example.SummerBuild.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.persistence.*;
-import java.time.OffsetDateTime;
-import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "events")
 public class Event {
@@ -25,5 +28,10 @@ public class Event {
 
   @Column(nullable = false)
   private OffsetDateTime date;
+
+  public Event(String name, OffsetDateTime date) {
+        this.name = name;
+        this.date = date;
+    }
 
   }
