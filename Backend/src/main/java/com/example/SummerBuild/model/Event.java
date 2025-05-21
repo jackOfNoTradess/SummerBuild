3ebca3cbb87a02277e7adf66dfd8fb9b8125b658
lpwee <pingwee99@gmail.com>
2025-05-21 08:49:09 +0000

    fixed lombok, still need generics

--- Backend/src/main/java/com/example/SummerBuild/model/Event.java
+++ Backend/src/main/java/com/example/SummerBuild/model/Event.java
@@ -1,14 +1,13 @@
 package com.example.SummerBuild.model;
 
-import java.time.OffsetDateTime;
-import java.util.UUID;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.time.OffsetDateTime;
+import java.util.UUID;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -30,8 +29,7 @@ public class Event {
   private OffsetDateTime date;
 
   public Event(String name, OffsetDateTime date) {
-        this.name = name;
-        this.date = date;
-    }
-
+    this.name = name;
+    this.date = date;
   }
+}
