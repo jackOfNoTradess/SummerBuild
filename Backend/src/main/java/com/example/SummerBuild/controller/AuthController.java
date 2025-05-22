d8f340027a21f73096b4c580e3880c55c51c0569
jingronggg <limj0279@e.ntu.edu.sg>
2025-05-22 12:57:47 +0000

    T-021: added logging

--- Backend/src/main/java/com/example/SummerBuild/controller/AuthController.java
+++ Backend/src/main/java/com/example/SummerBuild/controller/AuthController.java
@@ -1,13 +1,18 @@
 package com.example.SummerBuild.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
+  Logger logger = LoggerFactory.getLogger(AuthController.class);
+
   @RequestMapping("/")
   public String index() {
+    logger.info("Welcome to SummerBuild");
     return "Welcome to SummerBuild";
   }
 }
