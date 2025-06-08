08951938cad9e36a6136b82293faea0d742f6c53
jingronggg <limj0279@e.ntu.edu.sg>
2025-06-08 12:54:39 +0000

    chore: update capacity constrains

--- Backend/src/main/java/com/example/SummerBuild/dto/EventsDto.java
+++ Backend/src/main/java/com/example/SummerBuild/dto/EventsDto.java
@@ -3,7 +3,7 @@ package com.example.SummerBuild.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -34,7 +34,7 @@ public class EventsDto extends BaseDto {
     return this.hostUuid;
   }
 
-  @Positive(message = "Capacity must be a positive number")
+  @PositiveOrZero(message = "Capacity must be a positive number")
   private Integer capacity;
 
   @NotNull(message = "Start time is required")
